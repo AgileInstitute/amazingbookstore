3e5d1c14da01f5b1d29a8a26dcbd6c44835f833d
Chad <cdvanroekel@gmail.com>
2022-09-07 19:05:55 -0500

    Adding first tests

---
+++ AmazingBookstoreTests/DiscountCalculatorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AmazingBookstoreTests;
+
+[TestClass]
+public class DiscountCalculatorTests
+{
+    [TestMethod]
+    public void GivenMoney_WhenGivenDiscount_ThenDiscountIsApplied()
+    {
+        var money = 100;
+        var calculator = new DiscountCalculator();
+        Assert.AreEqual(90, calculator.Apply(money, 10));
+    }
+}
+
+public class DiscountCalculator
+{
+    public int Apply(int money, int i)
+    {
+        return 0;
+    }
+}
